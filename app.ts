245d5af4f2aff880c1b89e6d550ee982d6886e05
AsrullaT <asrullat@gmail.com>
2016-10-19 10:15:03 +0200

    mentioned some comments & removed unnecessary codeblocks

--- app.ts
+++ app.ts
@@ -1,21 +1,29 @@
 import * as Rx from 'rxjs/Rx';
+// entry file
+// delete whats below and write ur own script
 
+
+
+
+// sample Rx Observable - checking chars for duplicates
+let chars :string[] = "qwertzuiopü+^1234567890ß?=)(/&%$§°!asdfghjklöä#-.,mnbvcxy<>_:;QWERTZUqIOPÜÄÖLKJHGFSDAYXCVBNM".split('');
+let charsExisting = {};
 
 Rx.Observable
-    .interval(300)
-    .take(10)
+    .interval(100)
+    .take(chars.length)
+    .map((index :number) => chars[index])
+    .map((char :string, index :number)=> {
+        if (charsExisting[char]) {
+            return `${char} -- is a DUPLICATE!` 
+        }
+        else {
+            charsExisting[char] = true;
+            return `${char} -- no duplicate` 
+        }
+    })
     .subscribe(
         (x)=> console.log(x)
     )
-; 
+    ;
 
-let sample = "babylon";
-
-let check = (word :string)=> {
-    return !word.split('').some((letter :string, index :number)=> {
-        return word.split('').indexOf(letter) != index
-    })
-}
- 
- 
-console.log(check(sample));
